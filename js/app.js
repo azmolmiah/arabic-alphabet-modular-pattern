cde333c7b725f6007c739d4421b110a7b697a50e
Azmol-Miah <azmol@azmolmiah.com>
2019-05-23 14:53:48 +0100

    Second version using the module pattern

--- js/app.js
+++ js/app.js
@@ -1,85 +1,87 @@
 // ================================= STORAGE CONTROLLER ========================================= //
+const storageCtrl = (function() {
+  // Public Methods
+  return {
+    setStorageLetters: function() {
+      const data = letterCtrl.getData();
+      localStorage.setItem('pageNumber', JSON.stringify(data.current));
+    },
+    getStorageLetters: function() {
+      const data = letterCtrl.getData();
+      data.current = JSON.parse(localStorage.getItem('pageNumber'));
+      letterCtrl.getLetters(data.current);
+    },
+    setStorageLoopIndex: function(loopIndex, loopIndexCurrent) {
+      localStorage.setItem('loopIndex', JSON.stringify(loopIndex));
+      localStorage.setItem(
+        'loopIndexPageNumber',
+        JSON.stringify(loopIndexCurrent)
+      );
+    },
+    getStorageLoopIndex: function(current) {
+      const data = letterCtrl.getData();
+      if (
+        localStorage.getItem('loopIndex') !== null &&
+        current === JSON.parse(localStorage.getItem('loopIndexPageNumber'))
+      ) {
+        data.playIndex = JSON.parse(localStorage.getItem('loopIndex')) - 1;
+      } else {
+        data.playIndex = 0;
+      }
+    },
+    getData: function() {
+      const current = JSON.parse(localStorage.getItem('pageNumber'));
+      return current;
+    }
+  };
+})();
 
-// ================================= ITEM CONTROLLER ============================================ //
+// ================================= LETTER DATA CONTROLLER ============================================ //
 const letterCtrl = (function() {
   // letter constructor
   const Letters = function(letters) {
     this.letters = letters;
-  };
-  // Section Constructor
-  const Section = function(outputNum, sectionNum) {
-    this.outputNum = outputNum;
-    this.sectionNum = sectionNum;
   };
 
   // Data or Data Structure
   const data = {
     letters: letters,
     current: 0,
-    output: null,
-    outputTwo: null,
-    outputThree: null
+    output: '',
+    loopSound: new Audio(),
+    playIndex: 0,
+    playIndexArr: []
   };
 
   // Public Methods and variables
   return {
-    // Put letters into sections if needed on the page
-    sectionletters: function(pg) {
-      this.letters = pg;
-
-      // const sectionone = new Section(output, currentPage),
-      //   sectiontwo = new Section(outputTwo, sectionTwo),
-      //   sectionthree = new Section(outputThree, sectionThree);
-
-      if (this.letters.length == 2) {
-        // If page has two sectins
-        this.letters[0].sectOne.forEach(one => {
-          sectionone.sections(one);
-        });
-        this.letters[1].sectTwo.forEach(two => {
-          sectiontwo.sections(two);
-        });
-      } else if (this.letters.length == 3) {
-        // If page has three sections
-        this.letters[0].sectOne.forEach(one => {
-          sectionone.sections(one);
-        });
-        this.letters[1].sectTwo.forEach(two => {
-          sectiontwo.sections(two);
-        });
-        this.letters[2].sectThree.forEach(three => {
-          sectionthree.sections(three);
-        });
-      } else {
-        // if page has one section
-        uiCtrl.oneSectionOnly(pg);
-      }
-    },
+    // Output the letters from data
     getLetters: async function(pageNum) {
       const response = await fetch('js/qaida.json');
       const letters = await response.json();
       const obj = Object.values(letters);
       data.letters = obj;
       const getAllLetters = new Letters(data.letters[pageNum]);
-      letterCtrl.sectionletters(getAllLetters);
+      uiCtrl.output(getAllLetters);
+      uiCtrl.currentPageLetterSize(pageNum);
     },
     startQaida: function() {
       letterCtrl.getLetters(data.current);
+      storageCtrl.getStorageLoopIndex(data.current);
     },
-    nextPage: function() {
-      const uiSelectors = uiCtrl.getSelectors();
-      document.querySelector(uiSelectors.option).selectedIndex =
-        data.current + 1;
-      letterCtrl.getLetters(data.current + 1);
-      data.current++;
+    getNextPage: function() {
+      if (data.current === data.letters.length - 1) {
+        data.current = -1;
+      }
+      appCtrl.nextPage();
     },
-    prevPage: function() {
-      const uiSelectors = uiCtrl.getSelectors();
-      document.querySelector(uiSelectors.option).selectedIndex =
-        data.current - 1;
-      letterCtrl.getLetters(data.current - 1);
-      data.current--;
+    getPrevPage: function() {
+      if (data.current === 0) {
+        data.current = data.letters.length;
+      }
+      appCtrl.prevPage();
     },
+
     getData: function() {
       return data;
     }
@@ -92,25 +94,24 @@ const uiCtrl = (function() {
     letters: '#letters',
     option: '#options',
     next: '#next',
-    prev: '#prev'
+    prev: '#prev',
+    image: 'img',
+    playBtn: '#playBtn',
+    stopBtn: '#stopBtn',
+    setBookMark: '#setBookMark',
+    getBookMark: '#getBookMark'
   };
 
   // Public Methods and variables
   return {
-    // Populate letters when ther's more than one section on the page
-    section: function(letterNum) {
-      this.outputNum += `<td id="${letterNum.name}"><img src="
-      ${letterNum.image}"></td>`;
-      this.sectionNum.innerHTML = this.outputNum;
-    },
-    // Populate letters if there's  just the one section on the page
-    oneSectionOnly: function(pgOne) {
-      let output;
+    // Populate letters in UI
+    output: function(pgOne) {
+      let output = letterCtrl.getData().output;
       const currentPage = document.querySelector(uiSelectors.letters);
       for (let i = 0; i < pgOne.letters.length; i++) {
-        output += `<td id="${pgOne.letters[i].name}" ><img src="${
+        output += `<img id="${pgOne.letters[i].name}" src="img/${
           pgOne.letters[i].image
-        }"></td>`;
+        }.png">`;
         currentPage.innerHTML = output;
       }
     },
@@ -118,12 +119,71 @@ const uiCtrl = (function() {
     populateOptionNumbers: function() {
       const currentPageNumber = document.querySelector(uiSelectors.option);
       let options;
-      for (let i = 0; i <= 40; i++) {
+      for (let i = 0; i <= 26; i++) {
         options += `<option>${i}</option>`;
       }
       currentPageNumber.innerHTML += options;
     },
-    // Change the letter sizes based on
+    // Change the letter sizes
+    changeLetterSize: function(letterWidth, letterHeight) {
+      const letterImg = document.getElementsByTagName(uiSelectors.image);
+      const img = Array.from(letterImg);
+      img.forEach(image => {
+        image.style.width = letterWidth;
+        image.style.height = letterHeight;
+      });
+    },
+    // Change the letter sizes depending on current page
+    currentPageLetterSize: function(current) {
+      const data = letterCtrl.getData();
+      // Change letter size depending on page
+      if (data.letters[current] === data.letters[0]) {
+        // Page 0 letter sizes
+        uiCtrl.changeLetterSize('5.5rem', 'auto');
+      } else if (data.letters[current] === data.letters[1]) {
+        // Page 1 letter sizes
+        uiCtrl.changeLetterSize('4.58rem', '3.67rem');
+      } else if (data.letters[current] === data.letters[2]) {
+        // Page 2 letter sizes
+        uiCtrl.changeLetterSize('4.58rem', '3.3rem');
+      }
+    },
+    highlightLetter: function(index) {
+      const letterImg = document.getElementsByTagName(uiSelectors.image);
+      const img = Array.from(letterImg);
+      img[index].classList.add('bgBlue');
+      setTimeout(function() {
+        img[index--].classList.remove('bgBlue');
+      }, 1900);
+    },
+    pauseIcon: function() {
+      document.querySelector(uiSelectors.playBtn).classList.remove('fa-play');
+      document.querySelector(uiSelectors.playBtn).classList.add('fa-pause');
+    },
+    playIcon: function() {
+      document.querySelector(uiSelectors.playBtn).classList.remove('fa-pause');
+      document.querySelector(uiSelectors.playBtn).classList.add('fa-play');
+    },
+    bookMarkIcon: function(localStorageCurrent, current) {
+      if (localStorageCurrent !== current) {
+        document
+          .querySelector(uiSelectors.setBookMark)
+          .classList.remove('fas', 'fa-bookmark');
+        document
+          .querySelector(uiSelectors.setBookMark)
+          .classList.add('far', 'fa-bookmark');
+      } else {
+        document
+          .querySelector(uiSelectors.setBookMark)
+          .classList.remove('far', 'fa-bookmark');
+        document
+          .querySelector(uiSelectors.setBookMark)
+          .classList.add('fas', 'fa-bookmark');
+        document.querySelector(
+          uiSelectors.option
+        ).selectedIndex = storageCtrl.getData();
+      }
+    },
     getSelectors: function() {
       return uiSelectors;
     }
@@ -135,55 +195,163 @@ const appCtrl = (function(letterCtrl, uiCtrl) {
   // Load event listeners
   const loadEventlisteners = function() {
     // Get ui Selector(s)
-    const UISelectors = uiCtrl.getSelectors();
+    const uiSelectors = uiCtrl.getSelectors();
+
+    // Get next letter sound when current letter sound ends
+    const data = letterCtrl.getData();
+    data.loopSound.addEventListener('ended', nextSound);
+
+    // init Loop
+    document
+      .querySelector(uiSelectors.playBtn)
+      .addEventListener('click', initLoop);
+
+    // Stop Loop or Restart loop from beginning
+    document
+      .querySelector(uiSelectors.stopBtn)
+      .addEventListener('click', stopLoop);
+
+    //Get each letter sound
+    document
+      .querySelector(uiSelectors.letters)
+      .parentElement.addEventListener('click', individualSound);
+
     // Option
     document
-      .querySelector(UISelectors.option)
+      .querySelector(uiSelectors.option)
       .addEventListener('change', selectOptions);
 
     // Get next Button
     document
-      .querySelector(UISelectors.next)
-      .addEventListener('click', getNextPage);
+      .querySelector(uiSelectors.next)
+      .addEventListener('click', nextPage);
 
     // Get prev button
     document
-      .querySelector(UISelectors.prev)
-      .addEventListener('click', getPrevPage);
+      .querySelector(uiSelectors.prev)
+      .addEventListener('click', prevPage);
+
+    // Set local storage on current page
+    document
+      .querySelector(uiSelectors.setBookMark)
+      .addEventListener('click', setBookMark);
+
+    // Get local storage for saved page index
+    document
+      .querySelector(uiSelectors.getBookMark)
+      .addEventListener('click', getBookMark);
+  };
+
+  // Set book mark of just the page number in local storage
+  const setBookMark = function() {
+    storageCtrl.setStorageLetters();
+    const data = letterCtrl.getData();
+    uiCtrl.bookMarkIcon(storageCtrl.getData(), data.current);
+  };
+
+  // Get book mark of just the page number and feed to the output
+  const getBookMark = function() {
+    storageCtrl.getStorageLetters();
+    const data = letterCtrl.getData();
+    uiCtrl.bookMarkIcon(storageCtrl.getData(), data.current);
+    storageCtrl.getStorageLoopIndex(data.current);
+  };
+
+  // Get next letter sound in loop
+  const nextSound = function() {
+    const data = letterCtrl.getData();
+    if (data.playIndex === data.letters[data.current].length) {
+      uiCtrl.playIcon();
+      data.loopSound.pause();
+      stopLoop();
+    } else {
+      uiCtrl.highlightLetter(data.playIndex);
+      data.loopSound.src = `audio/${
+        data.letters[data.current][data.playIndex++].name
+      }.mp3`;
+      data.loopSound.play();
+    }
+  };
+
+  // Init Loop to play all letter sound
+  const initLoop = function() {
+    const data = letterCtrl.getData();
+    if (data.loopSound.paused) {
+      uiCtrl.pauseIcon();
+      nextSound();
+    } else {
+      // When clicking on pause button whilst looping
+      uiCtrl.playIcon();
+      data.loopSound.pause();
+      //Store current pause index
+      storageCtrl.setStorageLoopIndex(data.playIndex, data.current);
+    }
+  };
+
+  const stopLoop = function() {
+    const data = letterCtrl.getData();
+    data.playIndex = 0;
+    localStorage.removeItem('loopIndex');
+    localStorage.removeItem('loopIndexPageNumber');
+    data.loopSound.pause();
+    uiCtrl.playIcon();
+  };
+
+  // Play clicking individual sounds
+  const individualSound = function(e) {
+    const clickSound = new Audio();
+    // Get id name of each sound image thats outputed
+    clickSound.src = `audio/${e.path[0].id}.mp3`;
+    if (clickSound.paused) {
+      clickSound.play();
+    }
   };
 
   // Options
   const selectOptions = function(e) {
-    const lettersData = letterCtrl.getData();
-    lettersData.current = Number(e.target.value);
-    letterCtrl.getLetters(lettersData.current);
+    const data = letterCtrl.getData();
+    data.current = Number(e.target.value);
+    letterCtrl.getLetters(data.current);
+    uiCtrl.bookMarkIcon(storageCtrl.getData(), data.current);
+    storageCtrl.getStorageLoopIndex(data.current);
   };
 
   // Got to next page
-  const getNextPage = function() {
-    const lettersData = letterCtrl.getData();
-    if (lettersData.current === lettersData.letters.length - 1) {
-      return (lettersData.current = -1);
-    }
-    letterCtrl.nextPage();
+  const nextPage = function() {
+    const data = letterCtrl.getData();
+    const uiSelectors = uiCtrl.getSelectors();
+    document.querySelector(uiSelectors.option).selectedIndex = data.current + 1;
+    letterCtrl.getLetters(data.current + 1);
+    data.current++;
+    uiCtrl.bookMarkIcon(storageCtrl.getData(), data.current);
+    storageCtrl.getStorageLoopIndex(data.current);
   };
 
   // Go to prev page
-  const getPrevPage = function() {
-    const lettersData = letterCtrl.getData();
-    if (lettersData.current === 0) {
-      lettersData.current = lettersData.length;
-    }
-    letterCtrl.prevPage();
+  const prevPage = function() {
+    const data = letterCtrl.getData();
+    const uiSelectors = uiCtrl.getSelectors();
+    document.querySelector(uiSelectors.option).selectedIndex = data.current - 1;
+    letterCtrl.getLetters(data.current - 1);
+    data.current--;
+    uiCtrl.bookMarkIcon(storageCtrl.getData(), data.current);
+    storageCtrl.getStorageLoopIndex(data.current);
   };
 
-  //   //Public Methods
+  //Public Methods
   return {
     init: function() {
+      const data = letterCtrl.getData();
+
       // Start qaida app
       letterCtrl.startQaida();
+
       // Populate Options Numbers
       uiCtrl.populateOptionNumbers();
+
+      // If first page bookmarked then show bookmark
+      uiCtrl.bookMarkIcon(storageCtrl.getData(), data.current);
+
       // Load event listeners
       loadEventlisteners();
     }
